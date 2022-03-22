a6b7d3278a4a6db806dbf8575d72ae38503415d9
Brandon <brandonwbarrantes@gmail.com>
2022-03-22 11:26:37 -0400

    finished

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// random number generation
 const randNum1 = Math.floor(Math.random() * 6) + 1;
 const randNum2 = Math.floor(Math.random() * 6) + 1;
 
+// random img selection
 document
   .querySelectorAll("img")[0]
   .setAttribute("src", "images/dice" + randNum1 + ".png");
-
 document
   .querySelectorAll("img")[1]
   .setAttribute("src", "images/dice" + randNum2 + ".png");
 
-if (document.querySelector("h1").style.visibility !== hidden) {
-  document.querySelector("h1").style.visibility = hidden;
-}
+if (randNum1 > randNum2) {
+  document.querySelector(".title").innerHTML = "Player 1 Wins";
+} else if (randNum1 < randNum2) {
+  document.querySelector(".title").innerHTML = "Player 2 Wins";
+} else document.querySelector(".title").innerHTML = "Draw";
